e678d3169d2ed5257a4182c6e0b37c6d01237c3f
hudu <ahmedhudu21@gmail.com>
2025-11-19 20:21:22 +0100

    project structure

---
+++ app/lib/auth.ts
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from './prisma';
+import { compare } from 'bcrypt';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		CredentialsProvider({
+			name: 'Email / Password',
+			credentials: {
+				email: { label: 'Email', type: 'email' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials.password) {
+					return null;
+				}
+
+				const user = await prisma.user.findUnique({
+					where: { email: credentials.email },
+				});
+				if (!user) return null;
+
+				const isValid = await compare(credentials.password, user.password);
+				if (!isValid) return null;
+
+				return { id: user.id, email: user.email };
+			},
+		}),
+	],
+	session: {
+		strategy: 'jwt',
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+});
